8c44c13051958d20cdcf697b90092a7ee2bbfed6
Michon van Dooren <michon1992@gmail.com>
2014-12-11 01:00:24 -0800

    Initial version. This adds a 'Show all' button next to the 'Show more' button. This just keeps clicking 'Show more' until there is nothing more to show.

---
+++ gistfile1.js
@@ -0,0 +1,70 @@
+// ==UserScript==
+// @name         Pocketcasts Utils
+// @namespace    http://waxd.nl
+// @version      0.1
+// @description  Some utilities for pocketcasts
+// @author       MaienM
+// @match        https://play.pocketcasts.com/*
+// @grant        none
+// ==/UserScript==
+
+// Get the MutationObserver class for this browser.
+var MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+if (MutationObserver == null) {
+    console.error("Your piece of shit browser does not support MutationObserver.");
+}
+
+$(function() {
+    /*
+ * Add a show-all button next to the show-more button.
+ */
+    
+    // Create the button.
+    var showAll = $('<div class="show_all show_more">Show all</div>');
+    
+    // Handle it's click.
+    $(showAll).on('click', function() {
+        var showMore = $('div.show_more:not(.show_all)');
+        
+        // Every time the episodes list changes, click show more if there is still more to show.
+        var listObserver = new MutationObserver(function(mutations, observer) {
+            if ($(showMore).is(':visible')) {
+                $(showMore).click();
+            } else {
+            	listObserver.disconnect();
+            }
+        });
+        listObserver.observe($('#podcast_show div.episodes_list')[0], {
+            subtree: true,
+            childList: true,
+        });
+        
+        // Click it once to start.
+        $(showMore).click();
+    });
+    
+    // When needed, add it to the page.
+    var pageObserver = new MutationObserver(function(mutations, observer) {
+        var showMore = $('.show_more');
+        if ($(showMore).length > 0 && $('.show_all').length == 0) {
+            // Add the button.
+            $(showMore).after(showAll);
+            
+            // When the more button's visiblity changes, update the visibility of the all button as well.
+            var showObserver = new MutationObserver(function(mutations, observer) {
+                if ($(showMore).is(':visible')) {
+                    $(showAll).show();
+                } else {
+                    $(showAll).hide();
+                }
+            });
+            showObserver.observe(showMore[0], {
+                attributes: true,
+            });
+        }
+    });
+    pageObserver.observe($('#content_middle')[0], {
+        subtree: true,
+        childList: true,
+    });
+});
