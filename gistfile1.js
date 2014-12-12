4f822029f8f5e4756e482668ecf0902d733f58e0
Michon van Dooren <michon1992@gmail.com>
2014-12-12 06:09:46 -0800

    Moved the buttons to a menu on the side of the page. Added two buttons, one changing the order to the regular order, the other one changing the order to the reversed order.

--- gistfile1.js
+++ gistfile1.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         Pocketcasts Utils
-// @namespace    https://gist.github.com/MaienM/e477e0f4e8ec3c1836a7/raw/25712be7ef9e7008549b4e0aa9dff7bb3871f1fc/gistfile1.js
-// @updateURL    https://gist.githubusercontent.com/MaienM/e477e0f4e8ec3c1836a7/raw/25712be7ef9e7008549b4e0aa9dff7bb3871f1fc/gistfile1.js
-// @version      0.2
+// @namespace    https://gist.github.com/MaienM/e477e0f4e8ec3c1836a7
+// @updateURL    https://gist.githubusercontent.com/MaienM/e477e0f4e8ec3c1836a7/raw/
+// @version      0.3
 // @description  Some utilities for pocketcasts
 // @author       MaienM
 // @match        https://play.pocketcasts.com/*
@@ -15,7 +15,26 @@ if (MutationObserver == null) {
     console.error("Your piece of shit browser does not support MutationObserver.");
 }
 
+// Function to create a button.
+function createIcon(cls, description, callback) {
+    var btn = $('<div><i></i></div>');
+    var i = $(btn).find('i');
+    $(i).addClass('fa fa-lg fa-' + cls);
+    $(i).css('margin: 0 auto');
+    $(btn).css('display', 'inline-block');
+    $(btn).css('margin', '8px 0 5px 5px');
+    $(btn).css('min-width', '27px');
+    $(btn).css('min-height', '27px');
+    $(btn).css('border', '1px solid gray');
+    $(btn).attr('title', description);
+    $(btn).on('click', callback);
+    return btn;
+}
+
 $(function() {
+    // Add the fontawesome css to the header.
+    $('head').append($('<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">'));
+    
     /*
      * The basic show-more functionality.
      * 
@@ -41,7 +60,7 @@ $(function() {
        
     // Function to swap the order.
     var orderSwapped = false;
-    function doSwapOrder() {
+    function doOrderSwap() {
         // Get the div containing the episodes/headers.
         var container = $('#podcast_show .episodes_list > div:first');
         
@@ -59,10 +78,16 @@ $(function() {
         // Save the state.
         orderSwapped = !orderSwapped;
     }
-    
-    // Create the button.
-    var btnSwapOrder = $('<div class="swap_order show_more">Swap order</div>');
-	$(btnSwapOrder).on('click', doSwapOrder);
+    function doOrderRegular() {
+        if (orderSwapped) {
+            doOrderSwap();
+        }
+    }
+    function doOrderInverse() {
+        if (!orderSwapped) {
+            doOrderSwap();
+        }
+    }
     
 	/*
  	 * Add a show-all button next to the show-more button.
@@ -86,33 +111,56 @@ $(function() {
         doShowMore();
     }
     
-    // Create the button.
-    var btnShowAll = $('<div class="show_all show_more">Show all</div>');
-    $(btnShowAll).on('click', doShowAll);
+    /**
+     * Create buttons for all functions.
+     */
+    var iconOrderSwap = createIcon('sort', 'Swap order', doOrderSwap);
+    var iconOrderRegular = createIcon('sort-numeric-desc', 'Order newest -> oldest', doOrderRegular);
+    var iconOrderInverse = createIcon('sort-numeric-asc', 'Order oldest -> newest', doOrderInverse);
+    var iconShowMore = createIcon('', 'Show more', doShowMore);
+    var iconShowAll = createIcon('', 'Show all', doShowAll);
     
     /**
      * Add the buttons to the page when needed.
      */
+    var showObserver = null;
     var pageObserver = new MutationObserver(function(mutations, observer) {
+        // If the icon bar already exists, don't bother.
+        if ($('div.actions').length > 0) {
+            return;
+        }
+        
+        // Build an icon bar and put it below the image.
+        var container = $('<div></div>');
+        $(container).addClass('actions');
+        $('#podcast_header_image').append(container);
+        
+        // Add the icons.
+        $(container).append(iconOrderSwap);
+        $(container).append(iconOrderRegular);
+        $(container).append(iconOrderInverse);
+        $(container).append(iconShowMore);
+        $(container).append(iconShowAll);
+        
         // Get the show more button.
         var btnShowMore = $('.show_more');
-        if ($(btnShowMore).length > 0 && $('.show_all').length == 0) {
-            // Add the buttons.
-            $(btnShowMore).after(btnSwapOrder);
-            $(btnShowMore).after(btnShowAll);
             
-            // When the more button's visiblity changes, update the visibility of the all button as well.
-            var showObserver = new MutationObserver(function(mutations, observer) {
-                if ($(btnShowMore).is(':visible')) {
-                    $(btnShowAll).show();
-                } else {
-                    $(btnShowAll).hide();
-                }
-            });
-            showObserver.observe(btnShowMore[0], {
-                attributes: true,
-            });
+        // When the more button's visiblity changes, update the visibility of the associated buttons as well.
+        if (showObserver != null) {
+            showObserver.disconnect();
         }
+        showObserver = new MutationObserver(function(mutations, observer) {
+            if ($(btnShowMore).is(':visible')) {
+                $(iconShowMore).show();
+                $(iconShowAll).show();
+            } else {
+                $(iconShowMore).hide();
+                $(iconShowAll).hide();
+            }
+        });
+        showObserver.observe(btnShowMore[0], {
+            attributes: true,
+        });
     });
     pageObserver.observe($('#content_middle')[0], {
         subtree: true,
