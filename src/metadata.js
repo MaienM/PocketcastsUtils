7196937aa341a97a78388173ab1ab4fd38932a9b
Michon van Dooren <michon1992@gmail.com>
2016-01-28 23:06:51 +0100

    Removed the dist folder, instead moving towards a CI server

--- src/metadata.js
+++ src/metadata.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         Pocketcasts Utils
 // @namespace    https://github.com/MaienM/PocketcastUtils
-// @updateURL    http://waxd.nl/PocketcastsUtils.user.js
-// @version      2.0.0
+// @updateURL    http://waxd.nl/PocketcastsUtils-alpha.user.js
+// @version      2.0.0-alpha<%= grunt.template.today('yyyymmddHHMMssl') %>
 // @description  Enhance your Pocketcast experience with extra interface options, filters and other functionality
 // @author       MaienM
 // @match        https://play.pocketcasts.com/*
